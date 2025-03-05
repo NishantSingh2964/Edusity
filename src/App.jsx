14313c6295a338e5284cf854a38e6f0aebe14dae
NishantSingh2964 <nishantraj7859@gmail.com>
2025-03-05 18:51:01 +0530

    Initial Setup Complete

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+import About from './components/About/About'
+import Campus from './components/Campus/Campus'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import Hero from './components/Header/Hero'
+import Navbar from './components/Navbar/Navbar'
+import Programs from './components/Programs/Programs'
+import Testimonials from './components/Testimonials/Testimonials'
+import Title from './components/Title/title'
+import VideoPlayer from './components/VideoPlayer/VideoPlayer'
+
+function App() {
+
+  const [playState, setPlayState] = useState(false);
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <div className='container'>
+        <Title subTitle='OUR PROGRAM' title='What We Offer' />
+        <Programs />
+        <About setPlayState={setPlayState} />
+        <Title subTitle='Gallery' title='Campus Photos' />
+        <Campus />
+        <Title subTitle='TESTIMONIALS' title='What Students Say' />
+        <Testimonials />
+        <Title subTitle='Contact us' title='Get in Touch' />
+        <Contact />
+        <Footer />
+      </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
+    </>
+  )
+}
+
+export default App
